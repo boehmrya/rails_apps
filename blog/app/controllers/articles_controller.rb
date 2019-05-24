d52da96d75195c44922948b5be069d287977f30c
Ryan Boehm <ryan.w.boehm@gmail.com>
2019-05-24 18:34:20 -0400

    update action and edit template

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -7,23 +7,36 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-
-def new
-  @article = Article.new
-end
-
-def create
-  @article = Article.new(article_params)
-
-  if @article.save
-    redirect_to @article
-  else
-    render 'new'
+  def new
+    @article = Article.new
   end
-end
 
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
